27bfff3eeec1fbc8a40c81b005e214dc99507e6c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-25 15:23:12 +0530

    Cond to convert into gray

--- Assignment-1/changecolor.py
+++ Assignment-1/changecolor.py
@@ -9,3 +9,5 @@ else:
     gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)#convert to gray
     if gray is None:
         print("Could not convert to gray.")
+    else:
+        print("Image converted to gray successfully.")
