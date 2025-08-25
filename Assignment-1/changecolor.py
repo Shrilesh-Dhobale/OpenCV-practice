273b5068afd8f8a5db8f022e72657314be523389
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-25 15:24:58 +0530

    Condto save imgs added

--- Assignment-1/changecolor.py
+++ Assignment-1/changecolor.py
@@ -11,3 +11,6 @@ else:
         print("Could not convert to gray.")
     else:
         print("Image converted to gray successfully.")
+        print("Press 1 to save the gray image.")
+        print("Press 2 to exit without saving.")
+        select=int(input("Enter your choice: "))
