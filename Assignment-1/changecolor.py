69dd36c9888684509cc6aa93638a2a4be4315371
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-25 15:20:42 +0530

    Cond and conversion func added

--- Assignment-1/changecolor.py
+++ Assignment-1/changecolor.py
@@ -1,1 +1,9 @@
 import cv2
+
+image=cv2.imread('../img.jpg')#use to load img
+
+if image is None:
+    print("Could not read the image.")
+
+else:
+    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)#convert to gray
