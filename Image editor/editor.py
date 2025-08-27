709d508ebbcef529b8fc5c9b40611d73850aaf5e
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-27 18:16:27 +0530

    Cond added if img not found

--- Image editor/editor.py
+++ Image editor/editor.py
@@ -2,3 +2,5 @@ import cv2
 print("Welcome to Image Editor!!")
 select_image = input("Please select an image file: ")
 image = cv2.imread(select_image)  # use to load image
+if image is None:
+    print("Could not read the image.")
