5db8affa8f77a65a023e38181e2720a2e8b8a67a
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-27 18:17:33 +0530

    Img loaded and display

--- Image editor/editor.py
+++ Image editor/editor.py
@@ -4,3 +4,9 @@ select_image = input("Please select an image file: ")
 image = cv2.imread(select_image)  # use to load image
 if image is None:
     print("Could not read the image.")
+else:
+    print("Image loaded successfully.")
+    # Display the image
+    cv2.imshow("Loaded Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
