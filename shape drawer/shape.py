e80542bc2e192cb2421188b578b0b85b869c6bd0
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-29 16:12:07 +0530

    Press btn msg added

--- shape drawer/shape.py
+++ shape drawer/shape.py
@@ -1,3 +1,8 @@
 import cv2
 
 print("~~~~~~~~~~~~~~Welcome to Shape Drawer~~~~~~~~~~~~~~")
+print("Press 1 to draw a rectangle")
+print("Press 2 to draw a circle")
+print("Press 3 to draw a line")
+print("Press 4 to add text")
+print("Press 5 to exit")
