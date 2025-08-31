de34f542b4aaa7c83b3052975b298191a4b9a0be
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-31 10:51:17 +0530

    File name taken as input

--- video recorder/video.py
+++ video recorder/video.py
@@ -8,3 +8,4 @@ frame_width=int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height=int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 codec=cv2.VideoWriter_fourcc(*"XVID")
+output_file=input("Enter output file name(with extension): ")
