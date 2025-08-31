0156652ff63a7f418b9a4374adad097dde4d7597
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-31 10:57:35 +0530

    Video is loading

--- video recorder/video.py
+++ video recorder/video.py
@@ -10,3 +10,9 @@ frame_height=int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 codec=cv2.VideoWriter_fourcc(*"XVID")
 output_file=input("Enter output file name(with extension): ")
 fps=int(input("Enter frames per second: "))
+
+out=cv2.VideoWriter(output_file,codec,fps,(frame_width,frame_height))
+
+while True:
+    ret, frame = camera.read()
+    
