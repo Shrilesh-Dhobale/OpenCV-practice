a9a183d8cd7e571b4422f43e3b02c072ad555392
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-31 10:46:11 +0530

    Height taken as input

--- video recorder/video.py
+++ video recorder/video.py
@@ -5,3 +5,4 @@ print("-----------------Welcome tO Video Recorder------------------")
 camera=cv2.VideoCapture(0)
 
 frame_width=int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height=int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
