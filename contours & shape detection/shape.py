451475a721bb3a0a6455ac7ed10d86a65301f46a
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-02 11:09:58 +0530

    Img changed into black & white

--- contours & shape detection/shape.py
+++ contours & shape detection/shape.py
@@ -4,3 +4,4 @@ import numpy as np
 select_img=input("Enter image path: ")
 img=cv2.imread(select_img)
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+_,thresh=cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
