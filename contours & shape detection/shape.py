4f0a8f72f9cbd3edd3d90d75656bf7c60ffacc3f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-02 11:12:35 +0530

    Connect line where intensity same

--- contours & shape detection/shape.py
+++ contours & shape detection/shape.py
@@ -5,3 +5,5 @@ select_img=input("Enter image path: ")
 img=cv2.imread(select_img)
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 _,thresh=cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+
+contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
