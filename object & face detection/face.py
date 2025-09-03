d91be1fc286995014a98859ffe0e4e669728c43f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-03 12:16:19 +0530

    Img converted into gray

--- object & face detection/face.py
+++ object & face detection/face.py
@@ -2,3 +2,9 @@ import cv2
 
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 cap=cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)#Detect fast and accurate
+    
+   
