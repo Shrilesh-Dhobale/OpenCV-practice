1275a51793a151f8b256d800a15bf05e3e2b3f99
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-04 16:05:20 +0530

    Cond added for not smiling

--- object & face detection/face_smile_eyes.py
+++ object & face detection/face_smile_eyes.py
@@ -14,17 +14,19 @@ while True:
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-    roi_gray=gray[y:y+h,x:x+w]
-    roi_color=frame[y:y+h,x:x+w]
+        roi_gray=gray[y:y+h,x:x+w]
+        roi_color=frame[y:y+h,x:x+w]
 
+        eyes=eye_cascade.detectMultiScale(roi_gray,1.1,10)
+        # The condition should be > 0 to confirm detection.
+        if len(eyes) > 0:
+            cv2.putText(frame,"Eyes Detected",(x,y-30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
 
-    eyes=eye_cascade.detectMultiScale(roi_gray,1.1,5)
-    if len(eyes)>=0:
-        cv2.putText(frame,"Eyes Detected",(x,y-30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-
-    smiles=smile_cascade.detectMultiScale(roi_gray,1.7,22)
-    if len(smiles)>=0:
-        cv2.putText(frame,"Smiling",(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        smiles=smile_cascade.detectMultiScale(roi_gray,1.7,12)
+        if len(smiles) > 0:
+            cv2.putText(frame,"Smiling",(x,y-8),cv2.FONT_HERSHEY_SIMPLEX,1,(120,120,120),2)
+        if len(smiles) == 0:
+            cv2.putText(frame,"Not Smiling",(x,y-8),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
 
     cv2.imshow("Face Detection",frame)
     if cv2.waitKey(1) & 0xFF==ord('q'):
