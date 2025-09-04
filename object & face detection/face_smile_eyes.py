19a7b038c08d0a694e8b2fb3d036f0a91764b792
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-04 15:31:35 +0530

    Fun added to capture vdo

--- object & face detection/face_smile_eyes.py
+++ object & face detection/face_smile_eyes.py
@@ -3,3 +3,5 @@ import cv2
 face_cascade=cv2.CascadeClassifier("c:\\Users\\Shrilesh\\Desktop\\OpenCV practice\\object & face detection\\haarcascade_frontalface_default.xml")
 eye_cascade=cv2.CascadeClassifier("c:\\Users\\Shrilesh\\Desktop\\OpenCV practice\\object & face detection\\haarcascade_eye.xml")
 smile_cascade=cv2.CascadeClassifier("c:\\Users\\Shrilesh\\Desktop\\OpenCV practice\\object & face detection\\haarcascade_smile.xml")
+
+cap=cv2.VideoCapture(0)
