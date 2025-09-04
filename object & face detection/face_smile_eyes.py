e291c5d6cf92597aac0f6aebb76721eb425ac551
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-04 15:40:47 +0530

    Rectangle drawn for face

--- object & face detection/face_smile_eyes.py
+++ object & face detection/face_smile_eyes.py
@@ -11,3 +11,6 @@ while True:
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)#Detect fast and accurate
 
     faces=face_cascade.detectMultiScale(gray,1.1,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
